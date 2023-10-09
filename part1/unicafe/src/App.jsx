b4b10c8ed39ecf4d90adcfa53a842f7f7e1e4767
Jacek Różański <jacek@opcode.pl>
2023-10-09 21:10:23 +0200

    Exercise 1.11 - add handlers

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,13 +8,18 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const handleGoodClick = () => setGood(good + 1)
+    const handleNeutralClick = () => setNeutral(neutral + 1)
+    const handleBadClick = () => setBad(bad + 1)
+
+
     return (
         <>
             <div>
                 <h1>give feedback</h1>
-                <Button handleClick={() => setGood(good + 1)} text="good"/>
-                <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
-                <Button handleClick={() => setBad(bad + 1)} text="bad"/>
+                <Button handleClick={handleGoodClick} text="good"/>
+                <Button handleClick={handleNeutralClick} text="neutral"/>
+                <Button handleClick={handleBadClick} text="bad"/>
             </div>
 
             <Statistics good={good} neutral={neutral} bad={bad}/>
