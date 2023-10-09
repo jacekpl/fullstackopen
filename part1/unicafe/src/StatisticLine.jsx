19866f06cd5d7de232b4dc3ec48c1f46fe4abab2
Jacek Różański <jacek@opcode.pl>
2023-10-09 21:06:53 +0200

    Exercise 1.11

--- part1/unicafe/src/StatisticLine.jsx
+++ part1/unicafe/src/StatisticLine.jsx
@@ -1,5 +1,8 @@
 const StatisticLine = ({text, value}) => {
-    return <div>{text} {value}{text === 'positive' ? ' %' : ''}</div>
+    return <tr>
+        <td>{text}</td>
+        <td>{value}{text === 'positive' ? ' %' : ''}</td>
+    </tr>
 }
 
 export default StatisticLine;
