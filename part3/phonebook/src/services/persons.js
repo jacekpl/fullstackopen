3ca008890d797aeb192b9ed9eee3a37268711d5c
Jacek Różański <jacek@opcode.pl>
2023-10-14 23:55:05 +0200

    Exercise 3.11 (build is not pushed, but works)

--- part3/phonebook/src/services/persons.js
+++ part3/phonebook/src/services/persons.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = '/api'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
+    const request = axios.get(baseUrl + '/persons')
     return request.then(response => response.data)
 }
 
 const create = (newPerson) => {
-    return axios.post(baseUrl, newPerson).then(response => response.data)
+    return axios.post(baseUrl + '/persons', newPerson).then(response => response.data)
 }
 
 const update = (id, newPerson) => {
-    return axios.put(`${baseUrl}/${id}`, newPerson).then(response => response.data)
+    return axios.put(`${baseUrl}/persons/${id}`, newPerson).then(response => response.data)
 }
 
 const deletePerson = (id) => {
-    return axios.delete(`${baseUrl}/${id}`).then(response => response.data)
+    return axios.delete(`${baseUrl}/persons/${id}`).then(response => response.data)
 }
 
 export default {getAll, create, update, deletePerson}
