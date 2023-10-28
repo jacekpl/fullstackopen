bf02e97895ecf4b53e55b6a47c2b260c8234f532
Jacek Różański <jacek@opcode.pl>
2023-10-28 23:52:20 +0200

    Exercise 5.5

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login.js'
 import Notification from "./components/Notification.jsx";
+import LoginForm from "./components/LoginForm.jsx";
+import Togglable from "./components/Toggable.jsx";
+import BlogForm from "./components/BlogForm.jsx";
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
@@ -11,6 +14,7 @@ const App = () => {
     const [password, setPassword] = useState('')
     const [user, setUser] = useState(null)
     const [newBlog, setNewBlog] = useState({title: '', author: '', url: ''})
+    const [loginVisible, setLoginVisible] = useState(false)
 
     useEffect(() => {
         blogService.getAll().then(blogs =>
@@ -64,7 +68,7 @@ const App = () => {
         setNewBlog({...newBlog, url: event.target.value})
     }
 
-    const handleCreateBlog = async (event) => {
+    const addBlog = async (event) => {
         event.preventDefault()
 
         try {
@@ -80,24 +84,39 @@ const App = () => {
         }
     }
 
+    const blogForm = () => {
+        return (
+            <Togglable buttonLabel='new blog'>
+                <BlogForm
+                    newBlog={newBlog}
+                    handleTitleChange={handleTitleChange}
+                    handleAuthorChange={handleAuthorChange}
+                    handleUrlChange={handleUrlChange}
+                    addBlog={addBlog}/>
+            </Togglable>
+        )
+    }
+
+    const loginForm = () => {
+        return (
+            <Togglable buttonLabel='login'>
+                <LoginForm
+                    handleLogin={handleLogin}
+                    username={username}
+                    password={password}
+                    setUsername={setUsername}
+                    setPassword={setPassword}
+                />
+            </Togglable>
+        )
+    }
+
     if (user === null) {
         return (
             <div>
                 <h2>log in to application</h2>
                 <Notification message={message}/>
-                <form onSubmit={handleLogin}>
-                    <div>
-                        username
-                        <input type="text" value={username} name="Username"
-                               onChange={({target}) => setUsername(target.value)}/>
-                    </div>
-                    <div>
-                        password
-                        <input type="password" value={password} name="Password"
-                               onChange={({target}) => setPassword(target.value)}/>
-                    </div>
-                    <button type="submit">login</button>
-                </form>
+                {loginForm()}
             </div>
         )
     } else {
@@ -110,22 +129,7 @@ const App = () => {
 
                 <h2>create new</h2>
                 <Notification message={message}/>
-                <div>
-                    title
-                    <input type="text" value={newBlog.title} onChange={handleTitleChange}/>
-                </div>
-
-                <div>
-                    author
-                    <input type="text" value={newBlog.author} onChange={handleAuthorChange}/>
-                </div>
-
-                <div>
-                    url
-                    <input type="text" value={newBlog.url} onChange={handleUrlChange}/>
-                </div>
-
-                <button onClick={handleCreateBlog}>create</button>
+                {blogForm()}
 
                 <div>
                     {blogs.map(blog =>
