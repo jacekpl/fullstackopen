7cceded9655708a7a44a167bd967f9f4f7dc3d0f
Jacek Różański <jacek@opcode.pl>
2023-11-02 22:38:51 +0100

    Exercise 6.3

---
+++ part6/redux/src/App.jsx
@@ -0,0 +1,11 @@
+import NewNote from "./components/NewNote.jsx";
+import Notes from "./components/Notes.jsx";
+
+const App = () => {
+    return (<div>
+        <NewNote/>
+        <Notes/>
+    </div>)
+}
+
+export default App
