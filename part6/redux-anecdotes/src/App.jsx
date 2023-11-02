bd15e3f3778555d113b9e30f5d35cc103bd6bbe5
Jacek Różański <jacek@opcode.pl>
2023-11-02 22:44:54 +0100

    Exercise 6.4

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,35 +1,45 @@
-import { useSelector, useDispatch } from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import {asObject} from "./reducers/anecdoteReducer.js";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
 
-  const vote = (id) => {
-    dispatch({type: 'VOTE', data: { id }})
-    console.log('vote', id)
-  }
+    const vote = (id) => {
+        dispatch({type: 'VOTE', data: {id}})
+        console.log('vote', id)
+    }
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
+    const addAnecdote = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        dispatch({
+            type: 'NEW_ANECDOTE', data: asObject(content)
+        })
+    }
+
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            )}
+            <h2>create new</h2>
+            <form onSubmit={addAnecdote}>
+                <div><input name="anecdote"/></div>
+                <button>create</button>
+            </form>
         </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default App
