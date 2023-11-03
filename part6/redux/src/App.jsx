32295fd84a1c18871de8e8e5ce2b57143c6c823b
Jacek Różański <jacek@opcode.pl>
2023-11-03 17:21:37 +0100

    Notes update

--- part6/redux/src/App.jsx
+++ part6/redux/src/App.jsx
@@ -1,9 +1,15 @@
 import NewNote from "./components/NewNote.jsx";
 import Notes from "./components/Notes.jsx";
+import VisibilityFilter from "./components/VisibilityFilter.jsx";
 
 const App = () => {
+    const filterSelected = (value) => {
+        console.log(value)
+    }
+
     return (<div>
         <NewNote/>
+        <VisibilityFilter/>
         <Notes/>
     </div>)
 }
