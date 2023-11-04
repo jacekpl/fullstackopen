1073bb14759a641b39300a7f6e10f96a1688b0e1
Jacek Różański <jacek@opcode.pl>
2023-11-04 15:52:10 +0100

    Exercise 6.14

--- part6/redux/src/main.jsx
+++ part6/redux/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import {Provider} from "react-redux";
 import {configureStore} from "@reduxjs/toolkit";
 import App from "./App.jsx";
-import noteReducer, {createNote} from "./reducers/noteReducer";
-import filterReducer, {filterChange} from "./reducers/filterReducer";
+import noteReducer from "./reducers/noteReducer";
+import filterReducer from "./reducers/filterReducer";
 
 const store = configureStore({
     reducer: {
@@ -12,12 +12,6 @@ const store = configureStore({
         filter: filterReducer
     }
 })
-
-console.log(store.getState())
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
