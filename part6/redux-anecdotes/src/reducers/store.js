2ba4b281523b1a69a767d880728ae2373a4348e4
Jacek Różański <jacek@opcode.pl>
2023-11-04 09:34:35 +0100

    Exercise 6.12

--- part6/redux-anecdotes/src/reducers/store.js
+++ part6/redux-anecdotes/src/reducers/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import filterReducer from "./filterReducer.js";
 import anecdoteReducer from "./anecdoteReducer.js";
+import notificationReducer from "./notificationReducer.js";
 
 const store = configureStore({
     reducer: {
         anecdotes: anecdoteReducer,
-        filter: filterReducer
+        filter: filterReducer,
+        notification: notificationReducer
     }
 })
 
