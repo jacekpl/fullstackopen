b8101caaac4d19bd1ad5dab677a7fbb5d0044cee
Jacek Różański <jacek@opcode.pl>
2023-11-05 18:17:16 +0100

    Exercise 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,33 @@
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import axios from "axios";
+
 const AnecdoteForm = () => {
+    const queryClient = useQueryClient()
+    const newAnecdoteMutation = useMutation({
+        mutationFn: anecdote => axios.post('http://localhost:3001/anecdotes', anecdote).then(res => res.data),
+        onSuccess: (anecdote) => {
+            const anecdotes = queryClient.getQueryData(['anecdotes'])
+            queryClient.setQueryData(['anecdotes'], anecdotes.concat(anecdote))
+        }
+    })
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    const onCreate = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        console.log('new anecdote')
+        newAnecdoteMutation.mutate({content, votes: 0})
+    }
 
-  return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
+    return (
+        <div>
+            <h3>create new</h3>
+            <form onSubmit={onCreate}>
+                <input name='anecdote'/>
+                <button type="submit">create</button>
+            </form>
+        </div>
+    )
 }
 
 export default AnecdoteForm
