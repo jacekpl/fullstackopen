d40961e4c3aa5ad0348f003413f2035ed5c535ac
Jacek Różański <jacek@opcode.pl>
2023-11-12 21:37:55 +0100

    Exercise 7.11

--- part7/bloglist-frontend/src/main.jsx
+++ part7/bloglist-frontend/src/main.jsx
@@ -1,9 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import {NotificationContextProvider} from "./NotificationContext.jsx";
+import {QueryClient, QueryClientProvider} from "react-query";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <NotificationContextProvider>
-        <App/>
-    </NotificationContextProvider>
+    <QueryClientProvider client={queryClient}>
+        <NotificationContextProvider>
+            <App/>
+        </NotificationContextProvider>
+    </QueryClientProvider>
 );
