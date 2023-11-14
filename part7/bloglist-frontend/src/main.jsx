b5172791f0c4a2701b8df9179b9cdb5c320f94ba
Jacek Różański <jacek@opcode.pl>
2023-11-14 20:27:42 +0100

    Exercise 7.17

--- part7/bloglist-frontend/src/main.jsx
+++ part7/bloglist-frontend/src/main.jsx
@@ -3,6 +3,7 @@ import App from "./App";
 import {NotificationContextProvider} from "./NotificationContext.jsx";
 import {QueryClient, QueryClientProvider} from "react-query";
 import {UserContextProvider} from "./UserContext.jsx";
+import {BrowserRouter as Router} from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
@@ -10,7 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
         <UserContextProvider>
             <NotificationContextProvider>
-                <App/>
+                <Router>
+                    <App/>
+                </Router>
             </NotificationContextProvider>
         </UserContextProvider>
     </QueryClientProvider>
