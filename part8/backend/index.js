7f84e5a9f66eb2c176c690b184dde43be461460d
Jacek Różański <jacek@opcode.pl>
2023-11-23 23:51:59 +0100

    Exercise 8.6

--- part8/backend/index.js
+++ part8/backend/index.js
@@ -1,5 +1,6 @@
-const { ApolloServer } = require('@apollo/server')
-const { startStandaloneServer } = require('@apollo/server/standalone')
+const {ApolloServer} = require('@apollo/server')
+const {startStandaloneServer} = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
     {
@@ -117,6 +118,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book]
     allAuthors: [Author!]!
   }
+  
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -124,15 +134,15 @@ const resolvers = {
         bookCount: () => books.length,
         authorCount: () => authors.length,
         allBooks: (root, args) => {
-            if(!args) {
+            if (!args) {
                 return books
             }
 
-            if(args.author) {
+            if (args.author) {
                 books = books.filter(b => b.author === args.author)
             }
 
-            if(args.genre) {
+            if (args.genre) {
                 books = books.filter(b => b.genres.includes(args.genre))
             }
 
@@ -144,6 +154,16 @@ const resolvers = {
         bookCount: (root) => {
             return books.filter(b => b.author === root.name).length
         }
+    },
+    Mutation: {
+        addBook: (root, args) => {
+            const book = {...args, id: uuid()}
+            books = books.concat(book)
+            if(!authors.includes(book.author)){
+                authors = authors.concat({ name: book.author, id: uuid()})
+            }
+            return book
+        }
     }
 }
 
@@ -153,7 +173,7 @@ const server = new ApolloServer({
 })
 
 startStandaloneServer(server, {
-    listen: { port: 4000 },
-}).then(({ url }) => {
+    listen: {port: 4000},
+}).then(({url}) => {
     console.log(`Server ready at ${url}`)
 })
