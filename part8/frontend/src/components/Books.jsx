cd92c58e9966eecb472c1ddb4220fc0a0f7bffc8
Jacek Różański <jacek@opcode.pl>
2023-12-03 18:25:32 +0100

    Exercise 8.19

--- part8/frontend/src/components/Books.jsx
+++ part8/frontend/src/components/Books.jsx
@@ -1,22 +1,41 @@
 import {useQuery} from "@apollo/client";
-import {ALL_BOOKS} from "../queries.js";
+import {ALL_BOOKS, BOOKS_BY_GENRE} from "../queries.js";
+import {useState} from "react";
 
 const Books = (props) => {
     if (!props.show) {
         return null
     }
 
-    const result = useQuery(ALL_BOOKS)
+    const [genre, setGenre] = useState()
+    const variables = {};
+    const query = genre ? BOOKS_BY_GENRE : ALL_BOOKS
+
+    const result = useQuery(query, {
+        variables: genre ? {genre: genre} : null
+    })
 
     if(result.loading) {
         return <div>loading...</div>
     }
 
+    const uniqueFilter = (value, index, self) => {
+        return self.indexOf(value) === index;
+    }
+
     const books = result.data.allBooks
+    const genres = [];
+    for(const book of books) {
+        genres.push(...book.genres)
+    }
+
+    const uniqueGenres = genres.filter(uniqueFilter)
 
     return (
         <div>
             <h2>books</h2>
+
+            {genre && <div>in genre <strong>{genre}</strong></div>}
 
             <table>
                 <tbody>
@@ -34,6 +53,11 @@ const Books = (props) => {
                 ))}
                 </tbody>
             </table>
+
+            {uniqueGenres.map(genre => (
+                <button key={genre} onClick={() => setGenre(genre)}>{genre}</button>
+            ))}
+            <button key="all" onClick={() => setGenre(null)}>all genres</button>
         </div>
     )
 }
